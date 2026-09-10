ace6dac27787c15bb7c86ad87d6f9486c6111616
Kaeshko <thekaeshko@gmail.com>
2026-09-11 01:54:39 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,6 @@
-
-	local Event = game:GetService("ReplicatedStorage").ReplicatedModules.KnitPackage.Knit.Services.TraitService.RF.GetTraitsMetadata
-	local otvet = Event:InvokeServer()
-for i, v in pairs(otvet) do
-	print("key: "..i.." value: "..v)
-
-end
+local Event = game:GetService("ReplicatedStorage").ReplicatedModules.KnitPackage.Knit.Services.TraitService.RE.TraitHand
+Event.OnClientEvent:Connect(function(tabla)
+	for i, v in ipairs(tabla) do
+		print("trait "..i.." is "..v.Trait)
+	end
+end)
